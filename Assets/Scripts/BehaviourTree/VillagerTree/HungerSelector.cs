5e996abfee517f0f7c22d0706c8f0bc1bddb56b6
xTheDawg <sascha.ledermann@bluewin.ch>
2020-11-06 14:21:59 +0100

    Added base structure of Behaviour Tree

---
+++ Assets/Scripts/BehaviourTree/VillagerTree/HungerSelector.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HungerSelector : Selector
+{
+    public HungerSelector()
+    {
+    }
+}
---
+++ Assets/Scripts/BehaviourTree/VillagerTree/RootSelector.cs
@@ -0,0 +1,9 @@
+﻿public class RootSelector : Selector
+{
+    public RootSelector()
+    {
+        m_nodes.Add(new EnergySelector());
+        m_nodes.Add(new HungerSelector());
+        m_nodes.Add(new WorkSelector());
+    }
+}
