983367a55725215bcfd493ee2b58077e56daab34
xTheDawg <sascha.ledermann@bluewin.ch>
2020-11-15 12:55:22 +0100

    Added Resource Spawnscript, Added Collision Boxes to Prefabs

---
+++ Assets/Scripts/controller/ResourceSpawnController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceSpawnController : MonoBehaviour
+{
+    public GameObject treePrefab;
+
+    public GameObject stonePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            spawnResources(treePrefab);
+        }
+        for (int i = 0; i < 30; i++)
+        {
+            spawnResources(stonePrefab);
+        }
+    }
+    
+    public void spawnResources(GameObject toSpawn)
+    {
+        Instantiate(toSpawn, new Vector3(Random.Range(-130, 130), 0, Random.Range(-130, 130)),
+            new Quaternion(0, Random.Range(0, 360), 0, 0));
+    }
+}
