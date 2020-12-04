8b47ddaeeb5c6ce5e93e61c93a114f4ee1e35f4c
Simon Barben <simon.barben@gmail.com>
2020-12-04 12:54:56 +0100

    Failed to commit everything...

--- Assets/Scripts/BehaviourTree/VillagerTree/energy/actions/CheckEnergyAction.cs
+++ Assets/Scripts/BehaviourTree/VillagerTree/energy/actions/CheckEnergyAction.cs
@@ -4,7 +4,7 @@ public class CheckEnergyAction : ActionNode
 {
     public override NodeState Execute()
     {       
-        Debug.LogError("CheckEnergyAction()");
+        Debug.Log("CheckEnergyAction()");
         // Return success if peasant energyLevel is above critical level.
         return GetPeasant().energyLevel >= Globals.energyCritical ? NodeState.SUCCESS : NodeState.FAILURE;
     }
