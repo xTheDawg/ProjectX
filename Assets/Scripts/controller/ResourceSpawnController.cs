7955229379070b56032960263ed5394d2e812cb5
xTheDawg <sascha.ledermann@bluewin.ch>
2020-12-08 15:23:15 +0100

    Reworked Spawning method

--- Assets/Scripts/controller/ResourceSpawnController.cs
+++ Assets/Scripts/controller/ResourceSpawnController.cs
@@ -8,7 +8,11 @@ public class ResourceSpawnController : MonoBehaviour
     public GameObject treePrefab;
     public GameObject stonePrefab;
     private GameObject spawnedObject;
-    private ResourceHelper resourceHelper = new ResourceHelper();
+    private List<GameObject> activeObjects = new List<GameObject>();
+
+    private float objectCheckRadius = 5f;
+    private int maxSpawnAttemptsPerObject = 5;
+    private int maxSpawnRadius = 130;
 
     // Start is called before the first frame update
     void Start()
@@ -18,27 +22,61 @@ public class ResourceSpawnController : MonoBehaviour
         stonePrefab = Resources.Load("PT_Medieval_Rock_6") as GameObject;
         stonePrefab.AddComponent<StoneController>();
 
-        spawnResources(treePrefab, 100);
-        spawnResources(stonePrefab, 50);
+        SpawnResources(treePrefab, 100);
+        SpawnResources(stonePrefab, 50);
     }
 
-    public void spawnResources(GameObject toSpawn, int amount)
+    //Method to spawn Object in Scene
+    public void SpawnResources(GameObject toSpawn, int amount)
     {
         for (int i = 0; i < amount; i++)
         {
-            spawnedObject = Instantiate(toSpawn, new Vector3(Random.Range(-130, 130), 0, Random.Range(-130, 130)),
-                new Quaternion(0, Random.Range(0, 360), 0, 0));
+            //Create Position variable
+            Vector3 position = Vector3.zero;
 
-            //Destroy Object if it was spawned too close to the center
-            if (Vector3.Distance(spawnedObject.transform.position,Vector3.zero) < 10)
-                // || 
-                // Vector3.Distance(spawnedObject.transform.position,resourceHelper.FindClosestResource(spawnedObject.transform.position, ResourceType.WOOD).transform.position) < 4 ||
-                // Vector3.Distance(spawnedObject.transform.position,resourceHelper.FindClosestResource(spawnedObject.transform.position, ResourceType.STONE).transform.position) < 4 ||
-                // Vector3.Distance(spawnedObject.transform.position,resourceHelper.FindClosestResource(spawnedObject.transform.position, ResourceType.FOOD).transform.position) < 4)
+            //Whether or not we can spawn the Object
+            bool validPosition = false;
+
+            //Variable to track spawn attempts
+            int spawnAttempts = 0;
+
+            while (!validPosition && spawnAttempts < maxSpawnAttemptsPerObject)
             {
-                Debug.Log("Position invalid!");
-                Destroy(spawnedObject);
-                i--;
+                //Increment Attempt counter and switch validPosition flag
+                validPosition = true;
+                spawnAttempts++;
+                
+                //Pick a random position in an Area
+                position = new Vector3(Random.Range(-maxSpawnRadius, maxSpawnRadius), 0, Random.Range(-maxSpawnRadius, maxSpawnRadius));
+
+                //Array of all colliders within our Object check radius
+                Collider[] colliders = Physics.OverlapSphere(position, objectCheckRadius);
+
+                //Check valid position based on colliding tags
+                foreach (var collider in colliders)
+                {
+                    //A treeCollider is within the Radius
+                    if (collider.tag.Equals("Tree"))
+                    {
+                        validPosition = false;
+                    }
+                    //A stoneCollider is within the Radius
+                    if (collider.tag.Equals("Stone"))
+                    {
+                        validPosition = false;
+                    }
+                    //The position is too close to the center
+                    if (Vector3.Distance(position,Vector3.zero) < 50)
+                    {
+                        validPosition = false;
+                    }
+                }
+            }
+            
+            if (validPosition)
+            {
+                spawnedObject = Instantiate(toSpawn, position, new Quaternion(0, Random.Range(0, 360), 0, 0));
+                activeObjects.Add(spawnedObject);
             }
         }
     }
