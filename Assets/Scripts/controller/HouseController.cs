7c1bb65a47bf8402306e244491b6decd976976c7
xTheDawg <sascha.ledermann@bluewin.ch>
2020-12-09 17:22:57 +0100

    Modified Scene

---
+++ Assets/Scripts/controller/HouseController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseController : MonoBehaviour
+{
+    private ResourceSpawnController resourceSpawnController = GameObject.FindObjectOfType<ResourceSpawnController>();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        resourceSpawnController.SpawnObject(resourceSpawnController.peasantPrefab, Globals.storageLocation, resourceSpawnController.GetCenterRotation(Globals.storageLocation));
+    }
+}
